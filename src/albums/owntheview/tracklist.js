52570f40e660368c4a7760eee033787ea2252261
HelenLindberg <helenilindberg@yahoo.com>
2025-09-26 13:47:16 -0500

    added: own the view tracklist file

---
+++ src/albums/owntheview/tracklist.js
@@ -0,0 +1,98 @@
+import one from "./tracks/01GingerbreadMan.wav";
+import two from "./tracks/02SkyAsIs.wav";
+import three from "./tracks/03FastFoodHeaven.wav";
+import four from "./tracks/04FadedStar.wav";
+import five from "./tracks/05OwnTheView.wav";
+import six from "./tracks/06Resolutions.wav";
+import seven from "./tracks/07TiredOfSnow.wav";
+import eight from "./tracks/08GingerbreadMan.wav";
+import nine from "./tracks/09HappyNow.wav";
+import ten from "./tracks/10SlowNewsDay.wav";
+import cover from "../../assets/cover2.jpg"
+
+
+const tracks = [
+  {
+    name: "Gingerbread Man",
+    author: "The Brotherbears",
+    url: one,
+    id: 0,
+    image: cover,
+    color: "#00aeb0"
+  },
+  {
+      name: "Sky As Is",
+      author: "The Brotherbears",
+      url: two,
+      id: 1,
+      image: cover,
+      color: "#00aeb0"
+    },
+    {
+      name: "Fast Food Heaven",
+      author: "The Brotherbears",
+      url: three,
+      id: 2,
+      image: cover,
+      color: "#00aeb0"
+    },
+    {
+      name: "Faded Star",
+      author: "The Brotherbears",
+      url: four,
+      id: 3,
+      image: cover,
+      color: "#00aeb0"
+    },
+    {
+      name: "Own The View",
+      author: "The Brotherbears",
+      url: five,
+      id: 4,
+      image: cover,
+      color: "#00aeb0"
+    },
+    {
+      name: "Resolutions",
+      author: "The Brotherbears",
+      url: six,
+      id: 5,
+      image: cover,
+      color: "#00aeb0"
+    },
+    {
+      name: "Tired Of Snow",
+      author: "The Brotherbears",
+      url: seven,
+      id: 6,
+      image: cover,
+      color: "#00aeb0"
+    },
+    {
+      name: "Gingerbread Man (Painted Whore Remix)",
+      author: "The Brotherbears",
+      url: eight,
+      id: 7,
+      image: cover,
+      color: "#00aeb0"
+    },
+    {
+      name: "Happy Now",
+      author: "The Brotherbears",
+      url: nine,
+      id: 8,
+      image: cover,
+      color: "#00aeb0"
+    },
+    {
+      name: "Slow News Day",
+      author: "The Brotherbears",
+      url: ten,
+      id: 9,
+      image: cover,
+      color: "#00aeb0"
+    }
+];
+
+
+export default tracks;
