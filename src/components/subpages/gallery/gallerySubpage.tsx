5d178d0a784e15de7c0bee5714d31d427d7d7146
HelenLindberg <helenilindberg@yahoo.com>
2025-12-18 14:40:03 -0600

    borders on gallery pics

--- src/components/subpages/gallery/gallerySubpage.tsx
+++ src/components/subpages/gallery/gallerySubpage.tsx
@@ -5,6 +5,9 @@ import { Card } from "react-bootstrap";
 import './index.css'
 import circus from '../../../albums/circus/tracklist'
 import boys from '../../../albums/boys/tracklist'
+import skylines from '../../../albums/skylines/tracklist'
+import makingmyname from '../../../albums/makingmyname/tracklist'
+import owntheview from '../../../albums/owntheview/tracklist'
 
 export default function GallerySubpage() {
 
@@ -14,15 +17,15 @@ console.log(circus.color)
     <div className="gallery-subpage-outer" >
       
         <div className="gallery-wrapper">
-            <div style={{display: 'flex', flexDirection: 'column', gap: '30px'}}>
-        <Card className="gallery-card-style rounded-0  " style={{border: `2px solid ${circus.color}`}}>
+            <div style={{display: 'flex', flexDirection: 'column', gap: '40px'}}>
+        <Card className="gallery-card-style rounded-0  " style={{border: `2px solid #62b5b4`, boxShadow: '0 0 0 1px #2787ce'}}>
                 <Card.Body >
                     <Card.Img  variant="top" src={"https://rlhgqzuuag32i5so.public.blob.vercel-storage.com/photos/december16_2025/sixteen.jpeg"} />
                     {/* <Card.Title style={{ textAlign: 'center'}}>Music Soulmates</Card.Title> */}
                     
                 </Card.Body>
             </Card> 
-            <Card className="gallery-card-style rounded-0" style={{border: `2px solid ${boys.color}`}}>
+            <Card className="gallery-card-style rounded-0" style={{border: `2px solid #62b5b4`, boxShadow: '0 0 0 1px #2787ce'}}>
                 <Card.Body >
                     <Card.Img  variant="top" src={"https://rlhgqzuuag32i5so.public.blob.vercel-storage.com/photos/december16_2025/nineteen.jpeg"} />
                     {/* <Card.Title style={{ textAlign: 'center'}}>Music Soulmates</Card.Title> */}
@@ -30,15 +33,15 @@ console.log(circus.color)
                 </Card.Body>
             </Card> 
             </div>
-            <div style={{display: 'flex', flexDirection: 'column', gap: '30px'}}>
-            <Card className="gallery-card-style rounded-0 border-0">
+            <div style={{display: 'flex', flexDirection: 'column', gap: '40px'}}>
+            <Card className="gallery-card-style rounded-0 " style={{border: `2px solid #62b5b4`, boxShadow: '0 0 0 1px #2787ce'}}>
                 <Card.Body >
                     <Card.Img  variant="top" src={"https://rlhgqzuuag32i5so.public.blob.vercel-storage.com/photos/december16_2025/twentyone.jpeg"} />
                     {/* <Card.Title style={{ textAlign: 'center'}}>Music Soulmates</Card.Title> */}
                     
                 </Card.Body>
             </Card> 
-            <Card className="gallery-card-style rounded-0 border-0">
+            <Card className="gallery-card-style rounded-0" style={{border: `2px solid #62b5b4`, boxShadow: '0 0 0 1px #2787ce'}}>
                 <Card.Body >
                     <Card.Img  variant="top" src={"https://rlhgqzuuag32i5so.public.blob.vercel-storage.com/photos/december16_2025/seven.jpeg"} />
                     {/* <Card.Title style={{ textAlign: 'center'}}>Music Soulmates</Card.Title> */}
@@ -46,15 +49,15 @@ console.log(circus.color)
                 </Card.Body>
             </Card> 
             </div>
-            <div style={{display: 'flex', flexDirection: 'column', gap: '30px'}}>
-            <Card className="gallery-card-style rounded-0 border-0">
+            <div style={{display: 'flex', flexDirection: 'column', gap: '40px'}}>
+            <Card className="gallery-card-style rounded-0" style={{border: `2px solid #62b5b4`, boxShadow: '0 0 0 1px #2787ce'}}>
                 <Card.Body >
                     <Card.Img  variant="top" src={"https://rlhgqzuuag32i5so.public.blob.vercel-storage.com/photos/december16_2025/thirteen.jpeg"} />
                     {/* <Card.Title style={{ textAlign: 'center'}}>Music Soulmates</Card.Title> */}
                     
                 </Card.Body>
             </Card> 
-            <Card className="gallery-card-style rounded-0 border-0">
+            <Card className="gallery-card-style rounded-0 " style={{border: `2px solid #62b5b4`, boxShadow: '0 0 0 1px #2787ce'}}>
                 <Card.Body >
                     <Card.Img  variant="top" src={"https://rlhgqzuuag32i5so.public.blob.vercel-storage.com/photos/december16_2025/fourteen.jpeg"} />
                     {/* <Card.Title style={{ textAlign: 'center'}}>Music Soulmates</Card.Title> */}
